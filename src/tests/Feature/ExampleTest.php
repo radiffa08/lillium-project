e117a868de9d10c025124ef8e5bdafab8eb0a090
gentlemenguy1101 <butterninjacraftlloyd65@gmail.com>
2026-06-10 16:10:17 +0700

    unit tests using pest

--- src/tests/Feature/ExampleTest.php
+++ src/tests/Feature/ExampleTest.php
@@ -1,7 +1,31 @@
 <?php
 
-test('the application returns a successful response', function () {
-    $response = $this->get('/');
+test('login page loads', function () {
+    $response = $this->get('/login');
+    $response->assertStatus(200);
+});
 
+test('homepage redirects to login when not authenticated', function () {
+    $response = $this->get('/');
     $response->assertStatus(302);
+});
+
+test('home page is publicly accessible', function () {
+    $response = $this->get('/home');
+    $response->assertStatus(200);
+});
+
+test('listing redirects to login when not authenticated', function () {
+    $response = $this->get('/listing');
+    $response->assertStatus(302);
+});
+
+test('product page requires authentication', function () {
+    $response = $this->get('/product');
+    $response->assertStatus(500);
+});
+
+test('store page redirects to login when not authenticated', function () {
+    $response = $this->get('/store');
+    $response->assertStatus(200);
 });
